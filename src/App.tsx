fcd9f9ae3bedb43819b1cb6c6e11fed551870e59
mlopez <mlopez@student.42.fr>
2021-03-02 17:09:23 +0100

    starting pages and routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import {
     Switch,
     Route
 } from "react-router-dom";
+import HomePage from "./pages/home/home";
 
 function App() {
   return (
     <div className="App">
       <Router>
           <Switch>
-              <Route exact path="/">Main</Route>
+              <Route exact path="/">
+                  <HomePage />
+              </Route>
           </Switch>
       </Router>
     </div>
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,13 @@
+import {Component} from "react";
+
+export default class HomePage extends Component{
+
+    render() {
+        return (
+            <div>
+                Homepage yolo
+            </div>
+        )
+    }
+
+}
