fdaf596d5d1988df5ed4cb309db19d67542778dd
mlopez <mlopez@student.42.fr>
2021-03-05 12:20:57 +0100

    adding structure for cv page

---
+++ src/pages/curriculumVitae/curriculumVitae.tsx
@@ -0,0 +1,13 @@
+import {Component} from "react";
+import "./curriculumVitae.scss";
+
+export default class curriculumVitae extends Component {
+
+    render() {
+        return (
+            <div className="">
+
+            </div>
+        )
+    }
+}
