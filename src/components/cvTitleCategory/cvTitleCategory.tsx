c12f8b4d998004702d4905b2c00f304d0eea7c5a
mlopez <mlopez@student.42.fr>
2021-05-26 16:35:00 +0200

    Category title template working

--- src/components/cvTitleCategory/cvTitleCategory.tsx
+++ src/components/cvTitleCategory/cvTitleCategory.tsx
@@ -1,12 +1,25 @@
 import {Component} from "react";
 import "./cvTitleCategory.scss";
 
-export default class cvTitleCategory extends Component {
+type MyProps = {
+    comp: any,
+    title: String
+}
+
+export default class CvTitleCategory extends Component<MyProps> {
+    constructor(props: MyProps) {
+        super(props);
+    }
+
+    icon() {
+        return this.props.comp;
+    }
 
     render() {
         return (
             <div className="cv-title-category">
-
+                {this.icon()}
+                <div className="cv-title-category-text">{this.props.title}</div>
             </div>
         )
     }
--- src/pages/curriculumVitae/curriculumVitae.tsx
+++ src/pages/curriculumVitae/curriculumVitae.tsx
@@ -1,19 +1,30 @@
-import {Component} from "react";
+import {Component, Props} from "react";
 import "./curriculumVitae.scss";
+import CvTitleCategory from "../../components/cvTitleCategory/cvTitleCategory";
+import PersonIcon from '@material-ui/icons/Person';
+import PhoneIcon from '@material-ui/icons/Phone';
+import ChatIcon from '@material-ui/icons/Chat';
 
 export default class CurriculumVitae extends Component {
+    constructor(props: Props<any>) {
+        super(props);
+    }
 
     render() {
         return (
             <div className="cv-page">
                 <div className="cv-column">
                     <div>
-                        <p></p>
-                        <p></p>
+                        <CvTitleCategory comp={<PersonIcon />} title={'A PROPOS'}></CvTitleCategory>
+                        <div></div>
                     </div>
                     <div>
-                        <p></p>
-                        <p></p>
+                        <CvTitleCategory comp={<PhoneIcon />} title={'CONTACT'}></CvTitleCategory>
+                        <div></div>
+                    </div>
+                    <div>
+                        <CvTitleCategory comp={<ChatIcon />} title={'LANGUES'}></CvTitleCategory>
+                        <div></div>
                     </div>
                 </div>
                 <div className="cv-column">
