1fe5453f12cd3e97e815b66a62067d0bfc6ca5de
mlopez <mlopez@student.42.fr>
2021-07-02 17:03:40 +0200

    new titles, update cv page architecture, add cv header component

---
+++ src/components/cvHeader/cvHeader.tsx
@@ -0,0 +1,13 @@
+import {Component} from "react";
+import "./cvHeader.scss";
+
+export default class CvHeader extends Component {
+
+    render() {
+        return (
+            <div className="cv-header">
+
+            </div>
+        )
+    }
+}
--- src/pages/curriculumVitae/curriculumVitae.tsx
+++ src/pages/curriculumVitae/curriculumVitae.tsx
@@ -4,6 +4,9 @@ import CvTitleCategory from "../../components/cvTitleCategory/cvTitleCategory";
 import PersonIcon from '@material-ui/icons/Person';
 import PhoneIcon from '@material-ui/icons/Phone';
 import ChatIcon from '@material-ui/icons/Chat';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import SchoolIcon from '@material-ui/icons/School';
+import CvHeader from "../../components/cvHeader/cvHeader";
 
 export default class CurriculumVitae extends Component {
     constructor(props: Props<any>) {
@@ -13,28 +16,29 @@ export default class CurriculumVitae extends Component {
     render() {
         return (
             <div className="cv-page">
-                <div className="cv-column">
-                    <div>
-                        <CvTitleCategory comp={<PersonIcon />} title={'A PROPOS'}></CvTitleCategory>
-                        <div></div>
+                <CvHeader />
+                <div className="cv-page-content">
+                    <div className="cv-column">
+                        <div>
+                            <CvTitleCategory comp={<PersonIcon />} title={'A PROPOS'}></CvTitleCategory>
+                            <div></div>
+                        </div>
+                        <div>
+                            <CvTitleCategory comp={<PhoneIcon />} title={'CONTACT'}></CvTitleCategory>
+                            <div></div>
+                        </div>
+                        <div>
+                            <CvTitleCategory comp={<ChatIcon />} title={'LANGUES'}></CvTitleCategory>
+                            <div></div>
+                        </div>
                     </div>
-                    <div>
-                        <CvTitleCategory comp={<PhoneIcon />} title={'CONTACT'}></CvTitleCategory>
-                        <div></div>
-                    </div>
-                    <div>
-                        <CvTitleCategory comp={<ChatIcon />} title={'LANGUES'}></CvTitleCategory>
-                        <div></div>
-                    </div>
-                </div>
-                <div className="cv-column">
-                    <div>
-                        <p></p>
-                        <p></p>
-                    </div>
-                    <div>
-                        <p></p>
-                        <p></p>
+                    <div className="cv-column">
+                        <div>
+                            <CvTitleCategory comp={<BusinessCenterIcon />} title={'EXPERIENCES'}></CvTitleCategory>
+                        </div>
+                        <div>
+                            <CvTitleCategory comp={<SchoolIcon />} title={'FORMATION'}></CvTitleCategory>
+                        </div>
                     </div>
                 </div>
             </div>
