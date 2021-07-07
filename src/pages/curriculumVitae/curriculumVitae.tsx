88fdefeb18198e76116bbb8ae88d055ee85a40b3
mlopez <mlopez@student.42.fr>
2021-07-07 13:43:21 +0200

    cv page content add

--- src/pages/curriculumVitae/curriculumVitae.tsx
+++ src/pages/curriculumVitae/curriculumVitae.tsx
@@ -35,9 +35,53 @@ export default class CurriculumVitae extends Component {
                     <div className="cv-column">
                         <div>
                             <CvTitleCategory comp={<BusinessCenterIcon />} title={'EXPERIENCES'}></CvTitleCategory>
+                            <div className="cv-column-content">
+                                <div className="cv-column-content-item">
+                                    <div><div>D'Avril 2019</div><div>à Juin 2020</div></div>
+                                    <div>
+                                        <b>Auto-entreprenneur.fr</b>
+                                        <p>Developpeur front-end VueJs + Typescript. Refonte tunnel d'inscription, bureau virtuel et systeme d'édition de factures</p>
+                                    </div>
+                                </div>
+                                <div className="cv-column-content-item">
+                                    <div><div>D'Octobre 2018</div><div>à mars 2019</div></div>
+                                    <div>
+                                        <b>Lablaco</b>
+                                        <p>Developpeur front-end React. Réécriture du site en ReactJs, création d'un CRM complet (market place + gestion utilisateurs)</p>
+                                    </div>
+                                </div>
+                                <div className="cv-column-content-item">
+                                    <div><div>D'Avril 2017</div><div>à décembre 2017</div></div>
+                                    <div>
+                                        <b>Blupods Europe</b>
+                                        <p>Stagiaire développeur front-end Angular 4 + Typescript. Réécriture du proof of concept (codé en AngularJs) vers un site web fonctionnel et maintenable ainsi qu'ajout de fonctionnalités en Angular4</p>
+                                    </div>
+                                </div>
+                                <div className="cv-column-content-item">
+                                    <div><div>D'Avril 2014</div><div>à juin 2014</div></div>
+                                    <div>
+                                        <b>Association Française contre la Myopathie</b>
+                                        <p>Technicien support informatique. Suivi des utilisateurs et prise en charge des problèmes informatiques, tests de mise en place d'un systeme d'exploitation de remplacement</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div>
                             <CvTitleCategory comp={<SchoolIcon />} title={'FORMATION'}></CvTitleCategory>
+                            <div className="cv-column-content">
+                                <div className="cv-column-content-item">
+                                    <div>Depuis 2015</div>
+                                    <div><b>Étudiant à l'école 42</b><p>(<a href="https://www.42.fr" target="_blank">www.42.fr</a>)</p></div>
+                                </div>
+                                <div className="cv-column-content-item">
+                                    <div>2014</div>
+                                    <div><b>Niveau DUT Genie Électrique et Informatique Industrielle</b><p>(IUT Sénart Fontainebleau UPEC)</p></div>
+                                </div>
+                                <div className="cv-column-content-item">
+                                    <div>2012</div>
+                                    <div><b>BAC Scientifique option Sciences de l'ingénieur</b><p>(Lycée Rosa Parks à Montgeron)</p></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
