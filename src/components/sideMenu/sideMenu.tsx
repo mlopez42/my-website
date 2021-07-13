68666bf34d1e7fd028a9952c10340dc8e1cfeec9
mlopez <mlopez@student.42.fr>
2021-07-13 12:28:28 +0200

    moar css routing functionnal

--- src/components/sideMenu/sideMenu.tsx
+++ src/components/sideMenu/sideMenu.tsx
@@ -6,7 +6,8 @@ export default class SideMenu extends Component {
     render() {
        return (
         <div className="side-menu">
-            Coucou side menu
+            <a className="side-menu-href" href="/cv">Curriculum Vitae</a>
+            <a className="side-menu-href" href="/">Home</a>
         </div>
        )
     }
